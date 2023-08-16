bf52e257c36803c5b6913650f7c1c5e34b74bc84
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-08-15 23:08:22 -0400

    fixed route

--- views/CatEdit.jsx
+++ views/CatEdit.jsx
@@ -4,12 +4,11 @@ function CatEdit(props) {
     const cat = props.cat
   return (
     <div>
-         <a style={{color:'blue'}}  href='/dogs'>back</a>
+         <a style={{color:'blue'}}  href='/cats'>back</a>
         <form action={`/cats/${cat._id}?_method=PUT`}method='POST'>
         Name:<input type='text' name='name'defaultValue={cat.name}/><br/>
         Age:<input type='number' name='age'defaultValue={cat.age}/><br/>
-        HadFirstCheckUp:
-        { cat.HadFirstCheckUP ? <input type='checkbox' name='HadFirstCheckUP' defaultChecked />:<input type='checkbox' name='HadFirstCheckUP'/>}<br/>
+        First Check up:{ cat.HadFirstCheckUP ? <input type='checkbox' name='HadFirstCheckUP' defaultChecked />:<input type='checkbox' name='HadFirstCheckUP'/>}<br/>
         Description:<input type='text' name='description' defaultValue={cat.description}/><br/><br/>
         <input type='submit' name='' value='Update Cat'/>
         </form>
