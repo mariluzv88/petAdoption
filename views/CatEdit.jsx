4e9eafb36b8c8df3700f951f46b6506df0e57329
mariluzv88 <59103912+mariluzv88@users.noreply.github.com>
2023-08-15 23:17:53 -0400

    fixed edit checkbx bug

--- views/CatEdit.jsx
+++ views/CatEdit.jsx
@@ -8,7 +8,7 @@ function CatEdit(props) {
         <form action={`/cats/${cat._id}?_method=PUT`}method='POST'>
         Name:<input type='text' name='name'defaultValue={cat.name}/><br/>
         Age:<input type='number' name='age'defaultValue={cat.age}/><br/>
-        First Check up:{ cat.HadFirstCheckUP ? <input type='checkbox' name='HadFirstCheckUP' defaultChecked />:<input type='checkbox' name='HadFirstCheckUP'/>}<br/>
+        First Check up:  { cat.HadFirstCheckUp? <input type="checkbox" name="HadFirstCheckUp" defaultChecked />: <input type="checkbox" name="HadFirstCheckUp"/> }<br/>
         Description:<input type='text' name='description' defaultValue={cat.description}/><br/><br/>
         <input type='submit' name='' value='Update Cat'/>
         </form>
